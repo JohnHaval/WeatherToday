6cff31a9b4bde3fefbd80a9c1d4198916c088568
John Haval <acfowo45@gmail.com>
2022-12-18 19:50:56 +0300

    Доп. фиксация

--- WeatherUnitTest/WeatherFormTest.cs
+++ WeatherUnitTest/WeatherFormTest.cs
@@ -7,42 +7,71 @@ namespace WeatherUnitTest
     public class WeatherFormTest
     {
         [TestMethod]
-        public void CheckTimeState1()
+        public void __1CheckTimeState1()
         {
 
         }
         [TestMethod]
-        public void CheckTimeState2()
+        public void __2CheckTimeState2()
         {
 
         }
         [TestMethod]
-        public void CheckTimeState3()
+        public void __3CheckTimeState3()
         {
 
         }
         [TestMethod]
-        public void CheckTimeState4()
+        public void __4CheckTimeState4()
         {
 
         }
         [TestMethod]
-        public void CheckWeatherState1()
+        public void __5CheckWeatherState1()
         {
 
         }
         [TestMethod]
-        public void CheckWeatherState2()
+        public void __6CheckWeatherState2()
         {
 
         }
         [TestMethod]
-        public void CheckWeatherState3()
+        public void __7CheckWeatherState3()
         {
 
         }
         [TestMethod]
-        public void CheckWeatherState4()
+        public void __8CheckWeatherState4()
+        {
+
+        }
+        public void __9CheckFalloutState1()
+        {
+
+        }
+        [TestMethod]
+        public void _10CheckFalloutState2()
+        {
+
+        }
+        [TestMethod]
+        public void _11CheckFalloutState3()
+        {
+
+        }
+        [TestMethod]
+        public void _12CheckFalloutState4()
+        {
+
+        }
+        [TestMethod]
+        public void _13CheckFalloutState5()
+        {
+
+        }
+        [TestMethod]
+        public void _14CheckFalloutState6()
         {
 
         }
