7bac9c19b268f740b7de8717fdda6fbd774d1854
John Haval <acfowo45@gmail.com>
2022-12-18 19:50:21 +0300

    Наброшена последовательность тестов, проводимых для отчета

---
+++ WeatherUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WeatherUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WeatherUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f93239f9-d159-4d0b-8cd2-eeab5b43a08a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WeatherUnitTest/WeatherFormTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace WeatherUnitTest
+{
+    [TestClass]
+    public class WeatherFormTest
+    {
+        [TestMethod]
+        public void CheckTimeState1()
+        {
+
+        }
+        [TestMethod]
+        public void CheckTimeState2()
+        {
+
+        }
+        [TestMethod]
+        public void CheckTimeState3()
+        {
+
+        }
+        [TestMethod]
+        public void CheckTimeState4()
+        {
+
+        }
+        [TestMethod]
+        public void CheckWeatherState1()
+        {
+
+        }
+        [TestMethod]
+        public void CheckWeatherState2()
+        {
+
+        }
+        [TestMethod]
+        public void CheckWeatherState3()
+        {
+
+        }
+        [TestMethod]
+        public void CheckWeatherState4()
+        {
+
+        }
+    }
+}
